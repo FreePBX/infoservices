646fef2c541e946e6013210f73c56a372d7d4961
Micke Carlsson <mickecamino@gmail.com>
2008-09-11 20:26:34 +0000

    Enclosed text strings for localization in infoservice

--- install.php
+++ install.php
@@ -1,31 +1,35 @@
 <?php
-
+$directory = _("Directory");
 $fcc = new featurecode('infoservices', 'directory');
-$fcc->setDescription('Directory');
+$fcc->setDescription($directory);
 $fcc->setDefault('#');
 $fcc->update();
 unset($fcc);
 
+$calltrace = _("Call Trace");
 $fcc = new featurecode('infoservices', 'calltrace');
-$fcc->setDescription('Call Trace');
+$fcc->setDescription($calltrace);
 $fcc->setDefault('*69');
 $fcc->update();
 unset($fcc);	
 
+$echotest = _("Echo Test");
 $fcc = new featurecode('infoservices', 'echotest');
-$fcc->setDescription('Echo Test');
+$fcc->setDescription($echotest);
 $fcc->setDefault('*43');
 $fcc->update();
 unset($fcc);	
 
+$speakingclock = _("Speaking Clock");
 $fcc = new featurecode('infoservices', 'speakingclock');
-$fcc->setDescription('Speaking Clock');
+$fcc->setDescription($speakingclock);
 $fcc->setDefault('*60');
 $fcc->update();
 unset($fcc);	
 
+$speakextension = _("Speak Your Exten Number");
 $fcc = new featurecode('infoservices', 'speakextennum');
-$fcc->setDescription('Speak Your Exten Number');
+$fcc->setDescription($speakextension);
 $fcc->setDefault('*65');
 $fcc->update();
 unset($fcc);	
